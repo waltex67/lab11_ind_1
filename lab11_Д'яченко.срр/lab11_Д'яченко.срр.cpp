e76061752386a2659d37557ee92cf775493bf4a0
dacen <dacen@WALTEX>
2025-04-04 19:18:29 +0300

    Добавьте файлы проекта.

---
+++ lab11_Д'яченко.срр/lab11_Д'яченко.срр.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int a, b;
+
+    cout << "Введіть два цілих числа: ";
+    cin >> a >> b;
+
+    int* pa = &a;
+    int* pb = &b;
+
+    if (*pa > *pb) {
+        *pa = *pa * 5;
+        *pb = *pb - 5;
+    }
+    else {
+        *pb = *pb * 5;
+        *pa = *pa - 5;
+    }
+
+    cout << "Значення після обробки:\n";
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
